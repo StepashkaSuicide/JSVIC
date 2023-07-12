04d58ec02fa03b10cdb05fc9e8c1d131357e6691
engyS <churbakov.evgen@mail.ru>
2023-07-12 14:04:51 +0300

    les 4 full input

--- src/App.tsx
+++ src/App.tsx
@@ -2,105 +2,156 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 import {Button} from "./components/Button";
+import {FullInput} from "./components/FullInput";
 
 
 export type FilterType = 'All' | 'Active' | 'Completed'
 
 function App() {
 
-    let [tasks, setTasks] = useState([
-        {id: 0, title: "HTML&CSS", isDone: true},
-        {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS1", isDone: false},
-        {id: 4, title: "ReactJS2", isDone: false},
-        {id: 5, title: "ReactJS3", isDone: false},
+    let [messages, setMessages] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'}
     ])
 
-
-    const removeTask = (id: number) => {
-        setTasks(tasks.filter(el => el.id !== id))
-    }
-    // const ButtonFoo1 = (sub: string) => {
-    //     console.log(sub)
-    // }
-    // const ButtonFoo2 = (sub: string) => {
-    //     console.log(sub)
-    // }
-
-    type carsType = {
-        manufacturer: string
-        model: string
-    }
-
-    type moneyType = {
-        banknots: string
-        value: number
-        number: string
-    }
-    type moneyType1 = Array<moneyType>
-
-
-    const [money, setMoney] = useState<moneyType1>([
-        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-    ])
-
-    const filterMoneyDoll = () => {
-        console.log(money.filter(f => f.banknots === 'Dollars').map(m => m.banknots))
-    }
-    const filterMoneyRub = () => {
-
-    }
-
-    const topCars = [
-        {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'},
-        {manufacturer: 'Audi', model: 'rs6'}
-    ]
-
+const addMessage = (title: string)=> {
+        let newMessage = {message: title}
+        setMessages([newMessage, ...messages])
+}
 
     return (
-
-        <div className="App">
-            <Button name={'DOLL'} callBack={filterMoneyDoll}/>
-            <Button name={'RUB'} callBack={filterMoneyRub}/>
-
-            <ul>
-                {topCars.map((m, index) => {
-                    return (
-                        <li key={index}>
-                            {index+1}
-                            {m.model}
-                            {m.manufacturer}
-                        </li>
-                    )
-                })}
-            </ul>
-
-            <ul>
-                {money.filter(f => f.banknots !== 'Dollars').map(m => <li key={m.number}>{m.banknots}{m.value}{m.number}</li>)}
-
-            </ul>
-            <Todolist
-                title="What to learn"
-                tasks={tasks}
-                removeTask={removeTask}
-            />
+        <div>
+            <FullInput addMessage={addMessage}/>
+            {messages.map((f, index) => {
+                return (
+                    <div key={index}>{f.message}</div>
+                )
+            })}
         </div>
-    );
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let [tasks, setTasks] = useState([
+    //     {id: 0, title: "HTML&CSS", isDone: true},
+    //     {id: 1, title: "HTML&CSS", isDone: true},
+    //     {id: 2, title: "JS", isDone: true},
+    //     {id: 3, title: "ReactJS1", isDone: false},
+    //     {id: 4, title: "ReactJS2", isDone: false},
+    //     {id: 5, title: "ReactJS3", isDone: false},
+    // ])
+    //
+    //
+    // const removeTask = (id: number) => {
+    //     setTasks(tasks.filter(el => el.id !== id))
+    // }
+    // // const ButtonFoo1 = (sub: string) => {
+    // //     console.log(sub)
+    // // }
+    // // const ButtonFoo2 = (sub: string) => {
+    // //     console.log(sub)
+    // // }
+    //
+    // type carsType = {
+    //     manufacturer: string
+    //     model: string
+    // }
+    //
+    // type moneyType = {
+    //     banknots: string
+    //     value: number
+    //     number: string
+    // }
+    // type moneyType1 = Array<moneyType>
+    //
+    //
+    // const [money, setMoney] = useState<moneyType1>([
+    //     {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+    //     {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+    //     {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+    //     {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+    //     {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+    //     {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+    //     {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+    //     {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    // ])
+    //
+    // const filterMoneyDoll = () => {
+    //     console.log(money.filter(f => f.banknots === 'Dollars').map(m => m.banknots))
+    // }
+    // const filterMoneyRub = () => {
+    //
+    // }
+    //
+    // const topCars = [
+    //     {manufacturer: 'BMW', model: 'm5cs'},
+    //     {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+    //     {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+    //     {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+    //     {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+    //     {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+    //     {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+    //     {manufacturer: 'Mercedes', model: 'e63s'},
+    //     {manufacturer: 'Audi', model: 'rs6'}
+    // ]
+    //
+    //
+    // return (
+    //
+    //     <div className="App">
+    //         <Button name={'DOLL'} callBack={filterMoneyDoll}/>
+    //         <Button name={'RUB'} callBack={filterMoneyRub}/>
+    //
+    //         <ul>
+    //             {topCars.map((m, index) => {
+    //                 return (
+    //                     <li key={index}>
+    //                         {index+1}
+    //                         {m.model}
+    //                         {m.manufacturer}
+    //                     </li>
+    //                 )
+    //             })}
+    //         </ul>
+    //
+    //         <ul>
+    //             {money.filter(f => f.banknots !== 'Dollars').map(m => <li key={m.number}>{m.banknots}{m.value}{m.number}</li>)}
+    //
+    //         </ul>
+    //         <Todolist
+    //             title="What to learn"
+    //             tasks={tasks}
+    //             removeTask={removeTask}
+    //         />
+    //     </div>
+    // );
 }
 
 export default App;
