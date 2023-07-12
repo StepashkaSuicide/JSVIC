90f85416853f8c73b91a789b282aef650e56be66
engyS <churbakov.evgen@mail.ru>
2023-07-12 13:19:23 +0300

    les 3.1

--- src/App.tsx
+++ src/App.tsx
@@ -27,18 +27,73 @@ function App() {
     // const ButtonFoo2 = (sub: string) => {
     //     console.log(sub)
     // }
-  const ButtonFoo3 = (value: string) => {
-        console.log(value)
+
+    type carsType = {
+        manufacturer: string
+        model: string
     }
+
+    type moneyType = {
+        banknots: string
+        value: number
+        number: string
+    }
+    type moneyType1 = Array<moneyType>
+
+
+    const [money, setMoney] = useState<moneyType1>([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+
+    const filterMoneyDoll = () => {
+        console.log(money.filter(f => f.banknots === 'Dollars').map(m => m.banknots))
+    }
+    const filterMoneyRub = () => {
+
+    }
+
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'}, {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
+    ]
+
 
     return (
 
         <div className="App">
+            <Button name={'DOLL'} callBack={filterMoneyDoll}/>
+            <Button name={'RUB'} callBack={filterMoneyRub}/>
 
-            <Button name={'KEKL'} callBack={() => ButtonFoo3('jeka')}/>
-            <Button name={'rrrfds'} callBack={() => ButtonFoo3('lexa')}/>
-            {/*<Button name={'1'} callBack={ButtonFoo3}/>*/}
+            <ul>
+                {topCars.map((m, index) => {
+                    return (
+                        <li key={index}>
+                            {index+1}
+                            {m.model}
+                            {m.manufacturer}
+                        </li>
+                    )
+                })}
+            </ul>
 
+            <ul>
+                {money.filter(f => f.banknots !== 'Dollars').map(m => <li key={m.number}>{m.banknots}{m.value}{m.number}</li>)}
+
+            </ul>
             <Todolist
                 title="What to learn"
                 tasks={tasks}
